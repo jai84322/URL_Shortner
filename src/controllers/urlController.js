53304b5cd5c37e149da8bef21bddb09eb1495669
Kajal-gajjar <kngajjar123@gmail.com>
2022-07-18 14:03:00 +0530

    Get API

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,62 +1,99 @@
 const urlModel = require("../models/urlModel");
-const validUrl = require("valid-url")
-const shortId = require('shortid')
+const shortId = require("shortid");
+const axios = require("axios");
 
 const createShortURL = async function (req, res) {
   try {
-    let {longUrl, shortUrl, urlCode } = req.body
-    let obj = {}
+    let { longUrl, shortUrl, urlCode } = req.body;
+    let obj = {};
 
     if (Object.keys(req.body).length == 0) {
-      return res.status(400).send({status: false, message: "please enter valid request input"})
+      return res
+        .status(400)
+        .send({ status: false, message: "please enter valid request input" });
     }
 
     if (shortUrl || urlCode) {
-      return res.status(400).send({status: false, message: "please enter longUrl only"})
+      return res
+        .status(400)
+        .send({ status: false, message: "please enter longUrl only" });
     }
-    
+
     if (!longUrl) {
-      return res.status(400).send({status: false, message: "longUrl is missing"})
+      return res
+        .status(400)
+        .send({ status: false, message: "longUrl is missing" });
     }
 
-    if (!/(:?^((https|http|HTTP|HTTPS){1}:\/\/)(([w]{3})[\.]{1})?([a-zA-Z0-9]{1,}[\.])[\w]*((\/){1}([\w@?^=%&amp;~+#-_.]+))*)$/.test(longUrl)
-    ) {
-      return res.status(400).send({status: false, message: "please enter valid url"})
-    }
+    let found = false;
+    await axios
+      .get(longUrl)
+      .then((response) => {
+        if (response.status == 200 || response.status == 201) found = true;
+      })
+      .catch((err) => {});
 
-    // if (!validUrl.isUri(longUrl)) {
-    //   return res.status(400).send({status: false, message: "please enter valid url"})
-    // }
+    if (!found)
+      return res.status(400).send({ status: "false", message: "Invalid URL" });
 
-    let checkURL = await urlModel.findOne({longUrl : longUrl}).select({_id:0, __v:0})
+    let checkURL = await urlModel
+      .findOne({ longUrl: longUrl })
+      .select({ _id: 0, __v: 0 });
     if (checkURL) {
-      return res.status(201).send({status: true, message: "Url is already shortened", data: checkURL})
+      return res.status(201).send({
+        status: true,
+        message: "Url is already shortened",
+        data: checkURL,
+      });
     }
-    obj.longUrl = longUrl
+    obj.longUrl = longUrl;
+    obj.urlCode = shortId.generate();
 
-    let baseUrl = "http://localhost:3000/"
+    obj.shortUrl = "http://localhost:3000/".concat(
+      obj.urlCode.toLocaleLowerCase()
+    );
 
-    obj.urlCode = shortId.generate()
-
-    obj.shortUrl = baseUrl.concat(obj.urlCode.toLocaleLowerCase())
-
-
-    let savedData = await urlModel.create(obj)
-    let finalData = await urlModel.findOne(obj).select({_id: 0, __v:0})
-    return res.status(201).send({status: true, message: "success", data: finalData})
-
+    await urlModel.create(obj);
+    let finalData = await urlModel.findOne(obj).select({ _id: 0, __v: 0 });
+    return res
+      .status(201)
+      .send({ status: true, message: "success", data: finalData });
   } catch (err) {
     console.log(err);
     return res.status(500).send({ status: false, messgae: err.messgae });
   }
 };
- 
+
+const getURL = async function (req, res) {
+  try {
+    let urlCode = req.params.urlCode;
+
+    if (!shortId.isValid(urlCode))
+      return res
+        .status(400)
+        .send({ status: false, message: "Enter valid urlCode" });
+
+    let getLongURL = await urlModel.findOne({ urlCode: urlCode });
+
+    if (!getLongURL)
+      return res
+        .status(404)
+        .send({ status: false, message: "urlCode is not found" });
+
+    res
+      .status(302)
+      .redirect(getLongURL.longUrl, 302)
+      .send(`Found. Redirecting to ${getLongURL.longUrl}`);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ status: false, messgae: err.messgae });
+  }
+};
 
 module.exports = {
   createShortURL,
+  getURL,
 };
-
-
 
 // const shortUrl = `http://localhost:3000/${urlCode}`;
 //             const data = {};
@@ -69,5 +106,3 @@ module.exports = {
 //             await SET_ASYNC(`${urlCode}`, JSON.stringify(profile));
 //             let profile1=await urlModel.findOne({urlCode:urlCode}).select({ _id:0,createdAt: 0, updatedAt: 0, __v: 0 })
 //             res.status(201).send({ data: profile1 });
-
-
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {createShortURL} = require("../controllers/urlController")
+const { createShortURL, getURL } = require("../controllers/urlController");
 
 router.post("/url/shorten", createShortURL);
+router.get("/:urlCode", getURL);
+
+// validating the route
+router.all("/**", function (req, res) {
+  res.status(400).send({ status: false, message: "invalid http request" });
+});
 
 module.exports = router;
